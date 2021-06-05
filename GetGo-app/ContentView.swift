c668e5c468006139395c0532cd9510b515fe7cfb
Timotius Tjahjadi <timotiustjahjadi@gmail.com>
2021-06-05 22:05:15 +0700

    Initial Commit

---
+++ GetGo-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GetGo-app
+//
+//  Created by Timotius Tjahjadi  on 05/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
